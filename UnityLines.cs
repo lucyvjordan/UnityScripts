c5661203630d0ce41a90c23e861caa9e14df501f
Lucy <86916789+lucyvjordan@users.noreply.github.com>
2023-07-27 17:17:35 +0100

    Useful snippets of code

--- UnityLines.cs
+++ UnityLines.cs
@@ -7,6 +7,7 @@ playerObject.GetComponent<U2PlayerController>().DecreaseLives();
 // objects in the scene to prefabs in your project view which have not been put into scene yet
 // its not really recommended to use these Find methods at runtime, but I find they can be useful as a one off, especially if you use it in Start() methods
 
+
 [SerializeField] private BoxCollider[] colliderObjects;
 colliderObjects = FindObjectsOfType<BoxCollider>();
 foreach (Collider colliders in colliderObjects) {
@@ -16,3 +17,15 @@ foreach (Collider colliders in colliderObjects) {
 // used to find all objects in the scene with a particular type - eg colliders, scripts, renderers
 // used in this instance to find all objects with a box collider
 // foreach statement goes through each box collider in the array - means they can all be manipulated individually
+
+
+StartCoroutine(StartAnimation());
+IEnumerator StartAnimation() {
+    while (condition) {
+        statements
+        yield return null; }
+}
+
+// couroutines are used for functions which should run at the same time as other function
+// for example an event that occurs over a long period of time, using a while loop would cause other functions to freeze until that while loop had finished  
+// yield defines a place where the script will return to, allowing it to return to where it left off
